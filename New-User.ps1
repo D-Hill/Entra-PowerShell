c5ea5bed430f8c5f4d71e7a59a94d46cb8ad2924
D-Hill <duncanjameshill@gmail.com>
2026-07-12 11:11:38 +0100

    Added comment based help

--- New-User.ps1
+++ New-User.ps1
@@ -1,5 +1,65 @@
 
+<#
+.SYNOPSIS
+Creates a new Microsoft Entra ID user.
 
+.DESCRIPTION
+Creates a new user account in Microsoft Entra ID using Microsoft Graph PowerShell.
+Optional user attributes such as department, job title and office location can be supplied.
+
+.PARAMETER DisplayName
+The display name of the new user.
+
+.PARAMETER UserPrincipalName
+The User Principal Name (UPN) for the new user.
+
+.PARAMETER MailNickname
+The mail nickname (alias) for the new user.
+
+.PARAMETER GivenName
+The user's first name.
+
+.PARAMETER Surname
+The user's surname.
+
+.PARAMETER Password
+The initial password for the user.
+
+.PARAMETER ForceChangePasswordNextSignIn
+Determines whether the user must change their password at first sign-in.
+
+.PARAMETER AccountEnabled
+Determines whether the account is enabled after creation.
+
+.PARAMETER UsageLocation
+The user's usage location. Defaults to GB.
+
+.PARAMETER Department
+The user's department.
+
+.PARAMETER JobTitle
+The user's job title.
+
+.PARAMETER Office
+The user's office location.
+
+.PARAMETER GroupIds
+Optional array of Entra group IDs to add the user to.
+
+.EXAMPLE
+New-User -DisplayName "AN Other" `
+         -UserPrincipalName "AN.Other@contoso.com" `
+         -MailNickname "AN.Other"
+
+Creates a new Entra ID user.
+
+.NOTES
+Requires Microsoft Graph PowerShell permissions:
+- User.ReadWrite.All
+
+Author: D-Hill
+Version: 1.0
+#>
 function New-User {
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -54,17 +114,3 @@ function New-User {
     end {
     }
 }
-
-$NewUserParams = @{
-    DisplayName                  = 'Aidan Whitfield'
-    GivenName                    = 'Aidan'
-    Surname                      = 'Whitfield'
-    MailNickname                 = 'aidan.whitfield'
-    JobTitle                     = 'Chief Executive Officer'
-    Password                     = 'ChangeMe1!'
-    UserPrincipalName            = 'aidan.whitfield@duncanjameshillgmailcom.onmicrosoft.com'
-    Office                       = 'London'
-    ForceChangePasswordNextSignIn = $true
-}
-
-New-User @NewUserParams
